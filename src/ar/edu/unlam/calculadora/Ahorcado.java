b7538d5dbbf48157c1f0d3a3efbb9c2fcf6dbbc6
Macbook <macbook@MacBook-Pro-de-Macbook.local>
2024-08-20 18:07:16 -0300

    Agrego el codigo

--- src/ar/edu/unlam/calculadora/Ahorcado.java
+++ src/ar/edu/unlam/calculadora/Ahorcado.java
@@ -1,10 +1,40 @@
 package ar.edu.unlam.calculadora;
 
+import java.util.Scanner;
+
 public class Ahorcado {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner teclado = new Scanner(System.in);
+
+		Integer a;
+		Integer b;
+		Integer opcion;
+		Integer resultado;
+
+		do {
+			System.out.println(
+					"Ingrese su operacion /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para divivir");
+			opcion = teclado.nextInt();
+		} while (opcion < 1 && opcion > 4);
+
+		System.out.println("Ingrese el primer numero");
+		a = teclado.nextInt();
+		System.out.println("Ingrese el segundo numero");
+		b = teclado.nextInt();
+
+		if (opcion.equals(1)) {
+			resultado = a + b;
+		} else if (opcion.equals(2)) {
+			resultado = a - b;
+		} else if (opcion.equals(3)) {
+			resultado = a * b;
+		} else {
+			resultado = a / b;
+		}
+
+		System.out.println("El resultado es " + resultado.toString());
 	}
 
 }
